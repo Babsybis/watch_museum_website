ed29b3ca4bf50e49c4966c213e6774312e827c07
babsybis <babsybis@hotmail.fr>
2022-06-04 09:42:26 +0200

    Ajout munu burger

---
+++ script.js
@@ -0,0 +1,15 @@
+// //****************MENU BURGER****************
+const menuHamburger = document.querySelector(".menu_hamburger")
+const navLinks = document.querySelector(".nav_links")
+
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile_menu')
+})
+
+
+//********************MENU BURGER EN CROIX****************/
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', ()=> {
+    burger.classList.toggle('active');
+});
