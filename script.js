a5b5c1a81c5103283c16f55fe4a7cfbffdb965e8
Babsybis <babsybis@hotmail.fr>
2022-06-16 16:28:03 +0200

    Ajout horloge section croquis

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const menuHamburger = document.querySelector(".menu_hamburger")
 const navLinks = document.querySelector(".nav_links")
 
-menuHamburger.addEventListener('click',()=>{
+menuHamburger.addEventListener('click',() =>{
 navLinks.classList.toggle('mobile_menu')
 })
 
@@ -112,13 +112,14 @@ let i=1
 
  // ********************CROQUIS********************
  function myFunction() {
+  po3=document.querySelector('.position3');
+  po4=document.querySelector('.position4');
+
     if (window.matchMedia("(max-width: 500px)").matches) {
-        po3=document.querySelector('.position3');
         po3.classList.add('hidden');
-        po4=document.querySelector('.position4');
         po4.classList.add('hidden');  
     } else {
-      po3.classListadd('visible');
+        po3.classList.add('visible');
     }
   }
   myFunction()
@@ -159,6 +160,38 @@ x++
   function stopColor() {
     clearInterval(myInterval);
   }
+
+
+   // ********************HORLOGE********************
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+
+function setDate() {
+
+  const now = new Date();
+  const seconds = now.getSeconds(); // second hand rotation
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${
+  secondsDegrees}deg)`;
+  const mins = now.getMinutes(); // minutes hand rotation
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${
+  minsDegrees}deg)`;
+  const hour = now.getHours(); // Hours hand rotation
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${
+  hourDegrees}deg)`;
+
+
+  }
+
+setInterval(setDate, 1000);
+setDate();
+
+
 
 
 
