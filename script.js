46f0a9535bf01ce34d6a01588c010190ffdfbe6e
Babsybis <babsybis@hotmail.fr>
2022-06-21 14:15:24 +0200

    Simplification de la grille diapo

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ burger.addEventListener('click', ()=> {
 let pageContent = document.getElementById('pagecontent');
 
 // ********************DIV TEXTE********************
-let pictInfo = document.getElementById('pictinfo');
+let pictInfo = document.getElementById('pagecontent');
 
 // ********************CREATION JSON********************
 let rawImages = '[{"filename":"img1.png","name":"Le calendrier lunaire","author":"La phase de lune est en fait la plus vieille complication à avoir été utilisée par l’homme. Les montres à phase de lune sont étroitement liées aux montres à calendrier perpétuel. Elles sont inventées par Thomas Mudge en 1762 sous formes de montres à gousset, elles ne seront en revanche brevetées qu’en 1898 par Patek Philippe:Rolex introduit quant à elle sa première version de montre à phase de lune en 1949: il s’agit de la 8171. On peut la trouver notamment sur des marketplace comme Chrono 24 (d’où sont issues ces photos). Il s’agit d’un modèle Triple Calendar qu’on appelle « Padellone » (ce qui veut dire poêle en italien, en référence à son diamètre de 38mm très imposant pour l’époque). Le background bleu assez flamboyant a notamment inspiré la Cellini.","exposure":"Organisez votre visite"},{"filename":"img2.png","name":"La montre gousset","author":"La montre à gousset a vu le jour grâce à l’inventivité et l’esprit créatif de Jules Audemars et de l’aide de son associé Edward Piguet qui vont fonder en 1882 la Fondation de Audemars Piguet & cie. Cette fondation sera à l’origine de la première montre à gousset. Il existait bien avant la date de création de ce garde-temps universel de modèle de montre de poche mais qui n’avait pas de ressemblance avec les modèles que nous connaissons actuellement. C’est un instrument inscrit dans l\'histoire de nos civilisations depuis fort longtemps. Elles ont une fonction bien utile d\'indication de l\'heure, mais font aussi partie intégrantes des montres mixtes.","exposure":"Organisez votre visite"},{"filename":"img3.png","name":"Chronographe ou chronomètre ?","author":"Aujoud\'hui, l\'affirmation semble évidente. Cela n’a pas toujours été le cas: le chronographe ne s’est pas tout de suite porté, ni appelé de la sorte. L’invention de Nicolas Rieussec (1821) portait pourtant bien son nom : « chrono-graphe », elle, déposait littéralement une goutte d’encre sur un cadran de manière à visualiser le temps écoulé. Mais aujourd’hui, le chronomètre occupe à ses côtés une place de choix. Quelle différence ? La précision du chronomètre est garantie par une certification indépendante. Cette certification est le plus souvent donnée par le COSC, Contrôle Officiel Suisse des Chronomètres. D’autres institutions, à l’instar des observatoires, peuvent également l’attribuer, sous réserve que soient respectés les mêmes critères de tests.","exposure":"Organisez votre visite"},{"filename":"img4.png.jpg","name":"La montre bijou","author":"Bien que la montre-bracelet connaisse son avènement il y a un peu plus d’un siècle, on rapporte que la première fut créée en 1812.Suite à une commande de la sœur de Napoléon, Caroline Murat, Abraham-Louis Breguet, célèbre horloger, réalise sur mesure la Reine de Naples.Par la suite, la montre-bracelet, qui est imprécise du fait de sa petite taille, devient un accessoire réservé surtout à la gente féminine. C’est toujours dans la poche de la veste prévue à cet effet que les hommes gardent leur montre à gousset.","exposure":"Organisez votre visite"}]';
@@ -73,9 +73,9 @@ let i=1
         }, 1000)
         // console.log(i)
         document.querySelector(".photo").style.backgroundImage=`url("images/img`+i+`.png")`;
-        document.querySelector("#pictinfo h2").textContent=images[i-1].name;
-        document.querySelector("#pictinfo p").textContent=images[i-1].author;
-        document.querySelector("#pictinfo a").textContent=images[i-1].exposure;
+        document.querySelector("#pagecontent h2").textContent=images[i-1].name;
+        document.querySelector("#pagecontent p").textContent=images[i-1].author;
+        document.querySelector("#pagecontent a").textContent=images[i-1].exposure;
       
 
         afficherMasquer(i);
@@ -91,9 +91,9 @@ let i=1
         
             console.log(i)
             document.querySelector(".photo").style.backgroundImage=`url("images/img`+i+`.png")`;
-            document.querySelector("#pictinfo h2").textContent=images[i-1].name;
-            document.querySelector("#pictinfo p").textContent=images[i-1].author;
-            document.querySelector("#pictinfo a").textContent=images[i-1].exposure;
+            document.querySelector("#pagecontent h2").textContent=images[i-1].name;
+            document.querySelector("#pagecontent p").textContent=images[i-1].author;
+            document.querySelector("#pagecontent a").textContent=images[i-1].exposure;
             afficherMasquer(i);
     };
 
