1a002e603cf72f4d7e041f360269abaa2fc1245f
Babsybis <babsybis@hotmail.fr>
2022-07-06 14:55:15 +0200

    suppression des donnees via la methode get

---
+++ admin/delete.php
@@ -0,0 +1,34 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+// chargement du '.env'
+
+define('SERVER',$_ENV["HOST"]);
+define('DATABASE',$_ENV["BDD"]);
+define('USERNAME',$_ENV["USER"]);
+define('PASSWORD', $_ENV["PASSWORD"]);
+//variables environement
+
+$conn = "";
+try {
+    $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE, USERNAME, PASSWORD);
+    //je me connecte à ma database
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION);
+          //doit retourner erreur ou pas
+
+    $sth = $conn->prepare("DELETE FROM register_newsletter WHERE id=:id");
+    //prepare la commande sql
+    $sth -> bindParam(":id", $_GET['delete'], PDO::PARAM_INT);
+    //definition de la variable
+    $sth -> execute(["id" => $_GET['delete']]);
+    //execute la commande sql
+
+} catch(PDOException $e) {
+    echo "Connection failed: "  
+        . $e->getMessage();
+        //gestion des erreurs 
+}
+?>
