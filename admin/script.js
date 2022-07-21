d97467b7d088173ce36b61fb5897cf1599ec44b2
Babsybis <babsybis@hotmail.fr>
2022-07-21 16:14:29 +0200

    creation fonction permettant cration et suppression des lignes du tableau

--- admin/script.js
+++ admin/script.js
@@ -1,10 +1,51 @@
-
 //Création du tableau
 // On prépare une ligne pour le tableau
 const template = document.querySelector("#productrow");
 //Pour communiquer avec PHP
 let nombre = new FormData();
-nombre.append("dixdeplus", 0),
+nombre.append("dixdeplus", 0);
+
+//Fonction creation et supression ligne du tableau
+function rows(myJson) {
+    myJson.forEach(element => {
+        // On clone la ligne et on l'insère dans le tableau
+        let tbody = document.querySelector("tbody");
+        let clone = document.importNode(template.content, true);
+        let td = clone.querySelectorAll("td");
+        td[0].textContent = element["mail"];
+        td[1].textContent = element["date_location"];
+        td[2].setAttribute("data-value", element['id'])
+
+
+        tbody.appendChild(clone);   
+    });
+//Au click sur la poubelle
+let supp = document.querySelectorAll('.delete')
+let yesNo = document.querySelector('.popup')
+let yes = document.querySelector('.oui')
+let no = document.querySelector('.non')
+
+supp.forEach(element =>{
+element.addEventListener('click', function (){
+    yesNo.classList.add('visible')
+    no.addEventListener('click', function(){
+        yesNo.classList.remove('visible')
+    })
+    yes.addEventListener('click', function (){
+        const email = element.parentElement.parentElement
+        email.remove()
+        let data = new FormData();
+        data.append("delete", element.parentElement.dataset.value),
+        //Récuperer email à supprimer dans la bdd
+        fetch("delete.php", {
+            method: 'POST',
+            body: data 
+        })
+    yesNo.classList.remove('visible')
+    })
+    })  
+});
+}
 
     fetch("getdb.php", {
     method: 'POST',
@@ -13,54 +54,17 @@ nombre.append("dixdeplus", 0),
         return response.json();
     })
     .then(function(myJson) {
-        myJson.forEach(element => {
-            // On clone la ligne et on l'insère dans le tableau
-            let tbody = document.querySelector("tbody");
-            let clone = document.importNode(template.content, true);
-            let td = clone.querySelectorAll("td");
-            td[0].textContent = element["mail"];
-            td[1].textContent = element["date_location"];
-            td[2].setAttribute("data-value", element['id'])
-
-
-            tbody.appendChild(clone);   
-        });
-//Au click sur la poubelle
-let supp = document.querySelectorAll('.delete')
-let yesNo = document.querySelector('.popup')
-let yes = document.querySelector('.oui')
-let no = document.querySelector('.non')
-
-supp.forEach(element =>{
-    element.addEventListener('click', function (){
-        yesNo.classList.add('visible')
-        no.addEventListener('click', function(){
-            yesNo.classList.remove('visible')
-        })
-        yes.addEventListener('click', function (){
-            const email = element.parentElement.parentElement
-            email.remove()
-            let data = new FormData();
-            data.append("delete", element.parentElement.dataset.value),
-            //Récuperer email à supprimer dans la bdd
-            fetch("delete.php", {
-                method: 'POST',
-                body: data 
-            })
-        yesNo.classList.remove('visible')
-        })
-        })  
-    });
-})
+        rows(myJson)
+    })
 
 //Au click sur le bouton voir plus
 const show_more = document.querySelector('.btn_footer');
 console.log(show_more);
-let caca=0;
 show_more.addEventListener('click', function (){
-    caca=caca+10;
+    let delet = document.querySelectorAll('tbody tr').length
+    console.log(delet)
     let data = new FormData();
-    data.append("dixdeplus", caca),
+    data.append("dixdeplus", delet),
 
     fetch("getdb.php", {
         method: 'POST',
@@ -68,21 +72,14 @@ show_more.addEventListener('click', function (){
     .then(function(response) {
         return response.json();
     })
-    .then(function(myJson){
-        myJson.forEach(element => {
-            // On clone la ligne et on l'insère dans le tableau
-            let tbody = document.querySelector("tbody");
-            let clone = document.importNode(template.content, true);
-            let td = clone.querySelectorAll("td");
-            td[0].textContent = element["mail"];
-            td[1].textContent = element["date_location"];
-            td[2].setAttribute("data-value", element['id'])
-
-
-            tbody.appendChild(clone);   
-        });
+    .then(function(myJson) {
+        rows(myJson)
     })
 })
+        
+    
+
+
 
 
 
