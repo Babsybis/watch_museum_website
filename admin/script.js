4aeba9c4d9ad7e6143fb3c6982bd093fd64c0bd4
Babsybis <babsybis@hotmail.fr>
2022-07-26 16:37:41 +0200

    ajout du compteur

--- admin/script.js
+++ admin/script.js
@@ -5,6 +5,10 @@ const template = document.querySelector("#productrow");
 let nombre = new FormData();
 nombre.append("dixdeplus", 0);
 
+//variables
+const cpt_total = document.querySelector('.cpt_total')
+const cpt = document.querySelector('.cpt')
+
 //Fonction creation et supression ligne du tableau
 function rows(myJson) {
     myJson.forEach(element => {
@@ -42,6 +46,12 @@ element.addEventListener('click', function (){
             method: 'POST',
             body: data 
         })
+        //compteur encour de données bdd
+        .then(function(){
+            length = length - 1;
+            cpt.textContent = length;
+            cpt_total.textContent = cpt_total.textContent - 1;
+        })
     yesNo.classList.remove('visible')
     })
     })  
@@ -55,17 +65,19 @@ element.addEventListener('click', function (){
         return response.json();
     })
     .then(function(myJson) {
+        //compteur au départ sur nombre total
+        length = myJson.length
+        cpt.textContent = length
         rows(myJson)
     })
 
 //Au click sur le bouton voir plus
 const show_more = document.querySelector('.btn_footer');
-console.log(show_more);
 show_more.addEventListener('click', function (){
     let delet = document.querySelectorAll('tbody tr').length
-    console.log(delet)
     let data = new FormData();
     data.append("dixdeplus", delet),
+
 
     fetch("getdb.php", {
         method: 'POST',
@@ -74,6 +86,8 @@ show_more.addEventListener('click', function (){
         return response.json();
     })
     .then(function(myJson) {
+        length += myJson.length
+        cpt.textContent = length
         if (myJson.length <10 ){
             show_more.classList.remove('displayvisible');
         
@@ -90,14 +104,27 @@ const inputSearch = document.querySelector('.search');
 
 inputSearch.addEventListener('keyup', function(e){
     if (this.value == ""){
+        let tbody = document.querySelector("tbody");
+        tbody.querySelectorAll('tr').forEach(element => {
+            element.remove()
+        });
         show_more.classList.add('displayvisible')
+        fetch("getdb.php", {
+            method: 'POST',
+            body: nombre })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(myJson) {
+                rows(myJson)
+            })
+        return false;
+
     }else{
         show_more.classList.remove('displayvisible')
     }
-    if(this.value != ""){
 
-
-    if (e.key != "Enter"){
+    if (e.key != "Enter" && e.key != "altGraph" && e.key != "Control" && e.key != ""){
     let tbody = document.querySelector("tbody");
     tbody.querySelectorAll('tr').forEach(element => {
         element.remove()
@@ -113,25 +140,38 @@ inputSearch.addEventListener('keyup', function(e){
             return response.json();
         })
         .then(function(myJson) {
-
+            cpt.textContent = myJson.length
+            cpt_total.textContent = myJson.length
             rows(myJson)
         })
     }
 }
-    })
+)
 
     //Rendre le bouton voir plus invisible si moins de 10 données dans la bdd
     fetch('showmorevisible.php', {
     }).then(function (response) {
         return response.json();
         }).then(function (json) {
-        console.log(json)
+   
         if (json.length > 10) {
             show_more.classList.add('displayvisible');
         }
     })
 
+    //compteur total de données bdd
+    fetch("count.php", {
+        method: 'POST',
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            cpt_total.textContent = myJson[0][0]
+        })
 
+   
+        
 
 
 
