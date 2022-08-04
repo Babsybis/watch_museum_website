e2e1af79efa60cdab663f18d85fb98dff5f9eb55
Babsybis <babsybis@hotmail.fr>
2022-08-04 16:39:40 +0200

    ajout des chevrons svg

--- admin/index.php
+++ admin/index.php
@@ -49,8 +49,12 @@ if (!isset($_SESSION["login"])) {
         <table>
             <thead>
                 <tr class="first">
-                    <th>MAIL &nbsp; <img src="../images/haut.webp" alt="" width="15px"></th> 
-                    <th>DATE</th>
+                    <th>MAIL &nbsp; <svg class="tri" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.25 10.25L8 5.75L3.75 10.25" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></th> 
+                    <th>DATE&nbsp; <svg class="tri_date" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.25 10.25L8 5.75L3.75 10.25" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></th>
                     <th>DELETE</th>
                 </tr>
             </thead>
